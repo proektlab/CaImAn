cd0bac554eb02b18b75427ecb10a4206b1aa0bd8
Ethan Blackwood <ethanbblackwood@gmail.com>
2024-04-29 00:01:44 -0400

    Improve numbering and layout for my purposes for plot_contours and view_components

--- caiman/source_extraction/cnmf/estimates.py
+++ caiman/source_extraction/cnmf/estimates.py
@@ -241,6 +241,7 @@ class Estimates(object):
             caiman.utils.visualization.plot_contours(self.A[:, idx], img,
                                                      coordinates=coor_g,
                                                      display_numbers=display_numbers,
+                                                     inds_for_numbers=idx,
                                                      cmap=cmap)
             plt.title('Accepted Components')
             bad = list(set(range(self.A.shape[1])) - set(idx))
@@ -248,6 +249,7 @@ class Estimates(object):
             caiman.utils.visualization.plot_contours(self.A[:, bad], img,
                                                      coordinates=coor_b,
                                                      display_numbers=display_numbers,
+                                                     inds_for_numbers=bad,
                                                      cmap=cmap)
             plt.title('Rejected Components')
 
@@ -333,7 +335,7 @@ class Estimates(object):
             self.plot_contours(img=img, idx=idx, thr_method=thr_method,
                                thr=thr, params=params, cmap=cmap)
 
-    def view_components(self, Yr=None, img=None, idx=None) -> None:
+    def view_components(self, Yr=None, img=None, idx=None, show_spatial_component=True, display_inds=None) -> None:
         """view spatial and temporal components interactively
 
         Args:
@@ -366,14 +368,16 @@ class Estimates(object):
         if idx is None:
             caiman.utils.visualization.view_patches_bar(Yr, self.A, self.C,
                     self.b, self.f, self.dims[0], self.dims[1], YrA=self.R, img=img,
-                    r_values=self.r_values, SNR=self.SNR_comp, cnn_preds=self.cnn_preds)
+                    r_values=self.r_values, SNR=self.SNR_comp, cnn_preds=self.cnn_preds,
+                    show_spatial_component=show_spatial_component, display_inds=display_inds)
         else:
             caiman.utils.visualization.view_patches_bar(
                 Yr, self.A.tocsc()[:,idx], self.C[idx], self.b, self.f,
                 self.dims[0], self.dims[1], YrA=self.R[idx], img=img,
                 r_values=None if self.r_values is None else self.r_values[idx],
                 SNR=None if self.SNR_comp is None else self.SNR_comp[idx],
-                cnn_preds=None if np.sum(self.cnn_preds) in (0, None) else self.cnn_preds[idx])
+                cnn_preds=None if np.sum(self.cnn_preds) in (0, None) else self.cnn_preds[idx],
+                show_spatial_component=show_spatial_component, display_inds=display_inds)
 
     def nb_view_components(self, Yr=None, img=None, idx=None,
                            denoised_color=None, cmap='jet', thr=0.99) -> None:
