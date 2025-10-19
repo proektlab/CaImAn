9a61662f9d36e87166741d7e80d084adf24d999f
Ethan Blackwood <ethanbblackwood@gmail.com>
2025-10-18 22:28:01 -0400

    Fix bug with squeezing length-1 array in get_ROIs_to_merge

--- caiman/source_extraction/cnmf/merging.py
+++ caiman/source_extraction/cnmf/merging.py
@@ -52,7 +52,7 @@ def get_ROIs_to_merge(A: csc_matrix, C: np.ndarray, thr=0.85) -> tuple[list[np.n
     list_conxcomp = np.asarray(list_conxcomp_initial).T
 
     if list_conxcomp.ndim > 1:
-        cor = np.zeros((list_conxcomp.shape[1], 1))
+        cor = np.zeros(list_conxcomp.shape[1])
         for i in range(np.size(cor)):
             fm = np.where(list_conxcomp[:, i])[0]
             for j1 in range(np.size(fm)):
@@ -60,11 +60,11 @@ def get_ROIs_to_merge(A: csc_matrix, C: np.ndarray, thr=0.85) -> tuple[list[np.n
                     cor[i] = cor[i] + C_corr[fm[j1], fm[j2]]
         if np.size(cor) > 1:
             # we get the size (indices)
-            ind = np.argsort(np.squeeze(cor))[::-1]
+            ind = np.argsort(cor)[::-1]
         else:
             ind = [0]
         merged_ROIs = [np.where(list_conxcomp[:, i])[0] for i in ind]
-        corrs = np.squeeze(cor)[ind]
+        corrs = cor[ind]
     else:
         merged_ROIs = []
         corrs = np.array([])
